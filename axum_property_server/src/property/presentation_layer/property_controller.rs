d758fc1b4e45fe39b66e0eb5fc527e202b34ce04
arturs706 <aradionovs@yahoo.com>
2024-03-14 20:24:13 +0000

    first-commit

--- axum_property_server/src/property/presentation_layer/property_controller.rs
+++ axum_property_server/src/property/presentation_layer/property_controller.rs
@@ -9,11 +9,11 @@ use crate::property::application_layer::property_service;
 pub fn create_router() -> Router {
     Router::new()
         .route(
-            "/api/v1/property/upload_images/:id",
+            "/api/v1/properties/uploadimages/:id",
             post(property_service::upload_images).route_layer(DefaultBodyLimit::max(135476000)),
         )
-        .route("/api/v1/property/all", get(property_service::get_all_properties))
-        .route("/api/v1/property/:id", get(property_service::get_property_by_id))
-        .route("/api/v1/create_property", post(property_service::create_property))
+        .route("/api/v1/properties/getall", get(property_service::get_all_properties))
+        .route("/api/v1/properties/:id", get(property_service::get_property_by_id))
+        .route("/api/v1/properties/add", post(property_service::create_property))
         .layer(tower_http::trace::TraceLayer::new_for_http())
 }
